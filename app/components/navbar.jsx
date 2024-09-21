096670c00c0eb9cf290f7404763f1175d1bf55ec
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-22 03:46:36 +0500

    add: theme context

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,13 +1,26 @@
 // @flow strict
-import { userData } from "@/data/user-data";
 import Link from "next/link";
+import React from "react";
+import { userData } from "@/data/user-data";
+import ThemeContext from "../context/themeContext";
 
 function Navbar() {
+  const { theme, setTheme } = React.useContext(ThemeContext);
+
   return (
-    <nav className="bg-transparent hidden md:block">
+    <nav
+      className={`bg-transparent hidden md:block ${
+        theme ? "text-white" : "text-black"
+      }`}
+    >
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center opacity-0 md:opacity-100">
-          <Link href="/" className=" text-primary-icon text-3xl font-bold">
+          <Link
+            href="/"
+            className={`${
+              theme ? "text-primary-icon" : "text-purple-600"
+            } text-3xl font-bold`}
+          >
             @{userData.githubUser}
           </Link>
         </div>
@@ -16,14 +29,19 @@ function Navbar() {
           className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100"
           id="navbar-default"
         >
-          <label class="inline-flex items-center cursor-pointer rounded-md bg-green-700">
-            <input type="checkbox" value="" class="sr-only peer" />
-            <span class="block px-2 py-2 text-sm text-white transition-colors duration-300 hover:text-primary-icon">
-              Dark
-            </span>
-            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-primary-icon dark:peer-focus:text-primary-icon rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-icon" />
-            <span class="block px-2 py-2 text-sm text-white transition-colors duration-300 hover:text-primary-icon">
-              Light
+          <label
+            className={`inline-flex items-center cursor-pointer px-4 rounded-md ${
+              theme ? " bg-green-700" : "bg-green-300"
+            }`}
+          >
+            <input
+              type="checkbox"
+              onClick={() => setTheme(!theme)}
+              className="sr-only peer"
+            />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-primary-icon dark:peer-focus:text-primary-icon rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-icon" />
+            <span className="block px-2 py-2 text-sm transition-colors duration-300 hover:text-primary-icon">
+              {theme ? "Light" : "Dark"}
             </span>
           </label>
 
@@ -32,7 +50,7 @@ function Navbar() {
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
               href="/#projects"
             >
-              <div className="text-sm text-white transition-colors duration-300 hover:text-primary-icon">
+              <div className="text-smtransition-colors duration-300 hover:text-primary-icon">
                 Projects
               </div>
             </Link>
@@ -42,7 +60,7 @@ function Navbar() {
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
               href="#stats"
             >
-              <div className="text-sm text-white transition-colors duration-300 hover:text-primary-icon">
+              <div className="text-smtransition-colors duration-300 hover:text-primary-icon">
                 Stats
               </div>
             </Link>
@@ -52,7 +70,7 @@ function Navbar() {
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
               href="/#contributions"
             >
-              <div className="text-sm text-white transition-colors duration-300 hover:text-primary-icon">
+              <div className="text-smtransition-colors duration-300 hover:text-primary-icon">
                 Contributions
               </div>
             </Link>
--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,36 @@
-import { GoogleTagManager } from '@next/third-parties/google';
-import { Inter } from 'next/font/google';
-import Footer from './components/footer';
-import Navbar from './components/navbar';
-import './css/card.scss';
-import './css/globals.scss';
-const inter = Inter({ subsets: ['latin'] });
+"use client";
+import { GoogleTagManager } from "@next/third-parties/google";
+import { Inter } from "next/font/google";
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+import "./css/card.scss";
+import "./css/globals.scss";
+import ThemeContext from "./context/themeContext";
+import { useState } from "react";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const [theme, setTheme] = useState(false);
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-          <Navbar />
-          {children}
-        </main>
-        <Footer />
-      </body>
-      {
-        process.env.NEXT_PUBLIC_GTM &&
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <body
+          className={
+            inter.className +
+            ` ${theme ? "" : "bg-slate-300"} transition-all duration-300`
+          }
+        >
+          <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+            <Navbar />
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </ThemeContext.Provider>
+      {process.env.NEXT_PUBLIC_GTM && (
         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
-      }
+      )}
     </html>
-  )
-};
+  );
+}
